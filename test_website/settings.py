eca6430aca39857dc4e934333df0cf5a196c0356
Piyush Bansal <pbansal@thesummitgrp.com>
2024-01-20 13:57:25 +0530

    fixed errors

--- test_website/settings.py
+++ test_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8l_h^j2lg*ehe*!^_0udsi6f-(q!mg-rjy!97djpz6huit5b8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '127.0.0.1:8000', "*"]
 
 
 # Application definition
