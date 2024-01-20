44a63fcae5e32c8ae8b0afe1934761d84c3cccd8
Piyush Bansal <pbansal@thesummitgrp.com>
2024-01-20 17:00:14 +0530

    add cors

--- test_website/settings.py
+++ test_website/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '127.0.0.1:8000', "*"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+    "*",
+]
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'configurations',
+    'corsheaders',
     'rest_framework',
 ]
 
@@ -46,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
